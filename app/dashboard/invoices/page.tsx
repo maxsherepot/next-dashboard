fe2bea4ff9b0f18d4aaf587dd9520dba83f06a9c
Max <maksym@smartrr.com>
2025-07-25 19:11:11 +0300

    build fix

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,7 +7,7 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default async function Page({ searchParams }) {
+export default async function Page({ searchParams }: any) {
     const params = await searchParams
     const query = params?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
