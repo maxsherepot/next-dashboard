99758b215ebcea5851c5b95a61dd7d64846c79f6
Max <maksym@smartrr.com>
2025-07-30 13:38:35 +0300

    authenticate action added (Adding Authentication)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,11 @@
 'use server'
+
 import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -104,4 +107,23 @@ export async function deleteInvoice(id: string) {
     }
 
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
